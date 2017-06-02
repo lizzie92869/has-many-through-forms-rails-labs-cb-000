d8e362ddec3a99f3ff09b38c1586ae7625f1a968
Lizzie Sorin <lizzie.sorin@hotmail.fr>
2017-06-02 23:23:42 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
+  def user_attributes=(user_attributes)
+       if user_attributes[:username] != ""
+         self.user = User.create(user_attributes)
+       end
+  end
+  
 end
